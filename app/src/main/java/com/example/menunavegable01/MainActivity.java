0a36e3d38addeddbd538e3b61332af53aa8b94da
rb2711 <rodobertero@gmail.com>
2019-09-17 01:07:23 -0300

    actualizacion

--- app/src/main/java/com/example/menunavegable01/MainActivity.java
+++ app/src/main/java/com/example/menunavegable01/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.menunavegable01;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -50,9 +51,12 @@ public class MainActivity extends AppCompatActivity {
                 R.id.nav_tools, R.id.nav_share, R.id.nav_send)
                 .setDrawerLayout(drawer)
                 .build();
+
+        BottomNavigationView bottomNavigationView=findViewById(R.id.bottomNavigation);
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+        NavigationUI.setupWithNavController(bottomNavigationView,navController);
     }
 
     @Override
